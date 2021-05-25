440f91a8e2825b02562834072b3a3875f25082a8
jdmedlock <jdmedlock@gmail.com>
2021-05-25 09:40:27 -0500

    Refactor: Remove yaml files

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -34,25 +34,7 @@
 
 <nav class="bg-white shadow-sm">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="flex justify-between h-16">
-      <!--
-      <div class="flex">
-        <div class="flex-shrink-0 flex items-center">
-          <a href="/">
-            <img class="h-8 w-auto" src="/logo-192x192.png" alt="Chingu" />
-          </a>
-        </div>
-        <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-          <a
-            href="/dashboard"
-            class="{baseClasses}
-            { activeClasses }">
-            Dashboard
-          </a>
-        </div>
-      </div>
-      -->
-
+    <div class="flex justify-between">
       <div class="-mr-2 flex items-center sm:hidden">
         <!-- Mobile menu button -->
         <button
