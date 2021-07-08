c3318ac1a9b12931fdf420ce30ed498c21ca7d41
Jim Medlock <jdmedlock@gmail.com>
2021-07-08 18:55:27 -0500

    Feature: Update link to new Application Form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,7 +26,7 @@
   description="All self-directed developers run into the same problems. No team
   experience, feeling lost working alone, endless tutorials, not enough finished
   projects. The good news? Chingu solves them."
-  primaryHref="https://forms.gle/jYqoaMfJeW5Mw2r16"
+  primaryHref="https://forms.gle/56ANJu61c38tRoT48"
   primaryText="Apply for free"
   secondaryHref="/howItWorks"
   secondaryText="Learn more"
