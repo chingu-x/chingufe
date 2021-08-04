9164f91980a026b3019db283656cd43ec90fd503
jdmedlock <jdmedlock@gmail.com>
2021-08-04 09:02:01 -0500

    Feature: Update intro text in company list section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,10 +37,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="text-center">
         <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-          Innovative companies hire Chingu's
+          Leading companies hire Chingus
         </h2>
         <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-          Our members have found positions at some of the world's leading companies.
+          We've helped people get jobs at places like these:
         </p>
       </div>
       <div class="flow-root mt-8 lg:mt-10">
