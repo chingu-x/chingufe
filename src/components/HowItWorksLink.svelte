8735db4c1ff7d0d583f619368ea403c9d855981a
jdmedlock <jdmedlock@gmail.com>
2021-09-10 08:09:07 -0500

    Feature: Update color theme for How It Works button

--- src/components/HowItWorksLink.svelte
+++ src/components/HowItWorksLink.svelte
@@ -2,7 +2,7 @@
   href="/howItWorks" target="_blank"
   class="w-full flex items-center justify-center px-8 py-3
   border border-transparent text-base font-medium rounded-md
-  text-white bg-green-500 hover:bg-green-700 md:py-4
+  text-green-700 bg-green-100 hover:bg-green-200 md:py-4
   md:text-lg md:px-10">
   How it Works
 </a>
