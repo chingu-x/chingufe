f7732d47a57e07c5cdf12252c9769e32dc11cb38
jdmedlock <jdmedlock@gmail.com>
2024-04-04 19:08:43 -0500

    Refactor: Tune images and update configuration to enable text compression

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,8 @@ import tailwind from 'tailwindcss'
 import tailwindConfig from './tailwind.config.js'
 
 export default {
-    plugins: [autoprefixer, tailwind(tailwindConfig)]
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : { cssnano: {} })    }
 }
